38d83f593035f393c536fd5ac87a05c8e6da9974
andy <andy@andydote.co.uk>
2015-08-02 20:36:28 +0100

    overseer: redo structure slightly

---
+++ Overseer.RabbitMQ/MessageConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using RabbitMQ.Client.Events;
+
+namespace Overseer.RabbitMQ
+{
+	public class MessageConverter : IMessageConverter
+	{
+		public Message Convert(object input)
+		{
+			var message = input as BasicDeliverEventArgs;
+
+			if (message == null)
+			{
+				return null;
+			}
+
+			return new Message
+			{
+				Headers = new Dictionary<string, object>(),
+				Body = new object()
+			};
+		}
+	}
+}
---
+++ Overseer.RabbitMQ/MessageReader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Overseer.RabbitMQ
+{
+	public class MessageReader : IMessageReader
+	{
+		private readonly string _exchange;
+		private readonly IModel _channel;
+		private readonly QueueDeclareOk _queueName;
+		private Action _unhook;
+
+		public MessageReader(string hostName, string exchangeName)
+		{
+			_exchange = exchangeName;
+
+			var factory = new ConnectionFactory { HostName = hostName };
+			var connection = factory.CreateConnection();
+			
+			_channel = connection.CreateModel();
+			_channel.ExchangeDeclare(_exchange, ExchangeType.Topic, false, true, null);
+
+			_queueName = _channel.QueueDeclare();
+		}
+
+		public void Start(Action<object> onMessage)
+		{
+			var handler = new EventHandler<BasicDeliverEventArgs>((o, e) => onMessage(e));
+
+			var consumer = new EventingBasicConsumer(_channel);
+			consumer.Received += handler;
+
+			//string consumerTag = _channel.BasicConsume(_queueName, true, consumer);
+
+			_channel.QueueBind(_queueName, _exchange, ".");
+
+			_unhook = () => consumer.Received -= handler;
+		}
+
+		public void Stop()
+		{
+			_channel.QueueUnbind(_queueName, _exchange, ".", null);
+			_unhook();
+		}
+	}
+}
