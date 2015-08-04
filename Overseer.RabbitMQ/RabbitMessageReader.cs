ca0b7f3e42d12c784a1b5260be3555c2ab957622
andy <andy@andydote.co.uk>
2015-08-04 17:53:37 +0100

    rabbitMessageReader: refactor exchange and queue setup

--- Overseer.RabbitMQ/RabbitMessageReader.cs
+++ Overseer.RabbitMQ/RabbitMessageReader.cs
@@ -19,28 +19,27 @@ namespace Overseer.RabbitMQ
 			var connection = factory.CreateConnection();
 
 			_channel = connection.CreateModel();
-			_channel.ExchangeDeclare(_exchange, ExchangeType.Topic, false, true, null);
-
 			_queueName = _channel.QueueDeclare();
 		}
 
 		public void Start(Action<object> onMessage)
 		{
 			var handler = new EventHandler<BasicDeliverEventArgs>((o, e) => onMessage(e));
-
 			var consumer = new EventingBasicConsumer(_channel);
 			consumer.Received += handler;
-
-			//string consumerTag = _channel.BasicConsume(_queueName, true, consumer);
-
-			_channel.QueueBind(_queueName, _exchange, ".");
-
 			_unhook = () => consumer.Received -= handler;
+
+			_channel.ExchangeDeclare(_exchange, ExchangeType.Topic);
+
+			var queueName = _channel.QueueDeclare();
+
+			_channel.QueueBind(queueName, _exchange, "#");	// # = all
+			_channel.BasicConsume(queueName, false, consumer);
 		}
 
 		public void Stop()
 		{
-			_channel.QueueUnbind(_queueName, _exchange, ".", null);
+			_channel.QueueUnbind(_queueName, _exchange, "#", null);
 			_unhook();
 		}
 	}
