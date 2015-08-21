a449313c99d7a5cf686c6973faf76b66fac3e723
andy <andy@andydote.co.uk>
2015-08-21 17:55:26 +0100

    rabbitMessageReader: refactor

--- Overseer.RabbitMQ/RabbitMessageReader.cs
+++ Overseer.RabbitMQ/RabbitMessageReader.cs
@@ -9,7 +9,9 @@ namespace Overseer.RabbitMQ
 		private readonly RabbitOptions _options;
 		private readonly IModel _channel;
 		private QueueDeclareOk _queueName;
+
 		private Action _unhook;
+		private Action _hook;
 
 		public RabbitMessageReader(RabbitOptions options)
 		{
@@ -23,32 +25,53 @@ namespace Overseer.RabbitMQ
 
 		public void Start(Action<object> onMessage)
 		{
-			var handler = new EventHandler<BasicDeliverEventArgs>((o, e) => onMessage(e));
-			var consumer = new EventingBasicConsumer(_channel);
-			consumer.Received += handler;
+			Stop();
+			DeclareExchangeIfRequired();
 
-			_unhook = () => consumer.Received -= handler;
-
-			if (_options.ExchangeSkipDeclare)
-			{
-				_channel.ExchangeDeclare(
-					_options.ExchangeName,
-					ExchangeType.Topic,
-					_options.ExchangeDurable,
-					_options.ExchangeAutoDelete,
-					null);
-			}
+			var consumer = CreateConsumer(onMessage);
 
 			_queueName = _channel.QueueDeclare();
 
-			_channel.QueueBind(_queueName, _options.ExchangeName, _options.RoutingKey);	// # = all
+			_channel.QueueBind(_queueName, _options.ExchangeName, _options.RoutingKey);
 			_channel.BasicConsume(_queueName, false, consumer);
+
+			_hook();
 		}
 
 		public void Stop()
 		{
+			if (_unhook == null)
+				return;
+
 			_channel.QueueUnbind(_queueName, _options.ExchangeName, _options.RoutingKey, null);
 			_unhook();
+
+			_hook = null;
+			_unhook = null;
+		}
+
+		private void DeclareExchangeIfRequired()
+		{
+			if (_options.ExchangeSkipDeclare)
+				return;
+
+			_channel.ExchangeDeclare(
+				_options.ExchangeName,
+				ExchangeType.Topic,
+				_options.ExchangeDurable,
+				_options.ExchangeAutoDelete,
+				null);
+		}
+
+		private EventingBasicConsumer CreateConsumer(Action<object> onMessage)
+		{
+			var handler = new EventHandler<BasicDeliverEventArgs>((o, e) => onMessage(e));
+			var consumer = new EventingBasicConsumer(_channel);
+
+			_hook = () => consumer.Received += handler;
+			_unhook = () => consumer.Received -= handler;
+
+			return consumer;
 		}
 	}
 }
