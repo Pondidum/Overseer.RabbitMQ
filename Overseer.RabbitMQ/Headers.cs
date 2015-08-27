bf5d0a88101ec7cfe96c9d2f7496dc50b9d52254
andy <andy@andydote.co.uk>
2015-08-27 22:39:38 +0100

    rabbitMessageConverter: extract headerMapper to separate class to avoid rebuilding map multiple times

---
+++ Overseer.RabbitMQ/Headers.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RabbitMQ.Client;
+
+namespace Overseer.RabbitMQ
+{
+	public class Headers
+	{
+		private static readonly List<Getter> Map = BuildHeaderMapper();
+
+		public static Dictionary<string, object> FromProperties(IBasicProperties properties)
+		{
+			var headers = Map
+				.Where(g => g.HasValue(properties))
+				.ToDictionary(
+					g => g.Name,
+					g => g.GetValue(properties));
+
+			if (properties.IsHeadersPresent())
+			{
+				foreach (var pair in properties.Headers)
+				{
+					headers.Add(pair.Key, pair.Value);
+				}
+			}
+
+			return headers;
+		}
+
+		private static List<Getter> BuildHeaderMapper()
+		{
+			var type = typeof(IBasicProperties);
+			var methods = type.GetMethods();
+
+			var headers = new List<Getter>();
+
+			foreach (var property in type.GetProperties())
+			{
+				var checkMethod = methods.FirstOrDefault(m => m.Name == "Is" + property.Name + "Present");
+
+				if (checkMethod != null)
+				{
+					var getMethod = property.GetGetMethod();
+
+					Func<object, bool> check = input => (bool)checkMethod.Invoke(input, new object[] { });
+					Func<object, object> get = input => getMethod.Invoke(input, new object[] { });
+
+					headers.Add(new Getter
+					{
+						Name = property.Name,
+						HasValue = check,
+						GetValue = get
+					});
+				}
+			}
+
+			//remove custom cases
+			headers.RemoveAll(g => g.Name == "Headers");
+
+			return headers;
+		}
+
+		private struct Getter
+		{
+			public string Name;
+			public Func<object, bool> HasValue;
+			public Func<object, object> GetValue;
+		}
+	}
+}
--- Overseer.RabbitMQ/RabbitMessageConverter.cs
+++ Overseer.RabbitMQ/RabbitMessageConverter.cs
@@ -1,21 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using RabbitMQ.Client;
+﻿using System.Text;
 using RabbitMQ.Client.Events;
 
 namespace Overseer.RabbitMQ
 {
 	public class RabbitMessageConverter : IMessageConverter
 	{
-		private readonly IEnumerable<Getter> _headerMapper;
-
-		public RabbitMessageConverter()
-		{
-			_headerMapper = BuildHeaderMapper();
-		}
-
 		public Message Convert(object input)
 		{
 			var message = input as BasicDeliverEventArgs;
@@ -25,20 +14,7 @@ namespace Overseer.RabbitMQ
 				return null;
 			}
 
-			var headers = _headerMapper
-				.Where(g => g.HasValue(message.BasicProperties))
-				.ToDictionary(
-					g => g.Name,
-					g => g.GetValue(message.BasicProperties));
-
-			if (message.BasicProperties.IsHeadersPresent())
-			{
-				foreach (var pair in message.BasicProperties.Headers)
-				{
-					headers.Add(pair.Key, pair.Value);
-				}
-			}
-
+			var headers = Headers.FromProperties(message.BasicProperties);
 			var body = message.Body ?? new byte[0];
 			var json = Encoding.UTF8.GetString(body);
 
@@ -48,46 +24,6 @@ namespace Overseer.RabbitMQ
 				Headers = headers,
 				Body = json
 			};
-		}
-
-		private static IEnumerable<Getter> BuildHeaderMapper()
-		{
-			var type = typeof(IBasicProperties);
-			var methods = type.GetMethods();
-
-			var headers = new List<Getter>();
-
-			foreach (var property in type.GetProperties())
-			{
-				var checkMethod = methods.FirstOrDefault(m => m.Name == "Is" + property.Name + "Present");
-
-				if (checkMethod != null)
-				{
-					var getMethod = property.GetGetMethod();
-
-					Func<object, bool> check = input => (bool)checkMethod.Invoke(input, Type.EmptyTypes);
-					Func<object, object> get = input => getMethod.Invoke(input, Type.EmptyTypes);
-
-					headers.Add(new Getter
-					{
-						Name = property.Name,
-						HasValue = check,
-						GetValue = get
-					});
-				}
-			}
-
-			//remove custom cases
-			headers.RemoveAll(g => g.Name == "Headers");
-
-			return headers;
-		}
-
-		private struct Getter
-		{
-			public string Name;
-			public Func<object, bool> HasValue;
-			public Func<object, object> GetValue;
 		}
 	}
 }
